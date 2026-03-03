37e9b53b4bb55f65b0c270c90dc821141d06ab6d
Xavier Skapin <xavier.skapin@xlim.fr>
2026-03-03 20:39:12 +0100

    Update TP1

---
+++ TPs/TP1/TP1 Solar System/Assets/Scripts/ShowOnlyAttribute.cs
@@ -0,0 +1,9 @@
+// Assets > Scripts > ShowOnlyAttribute.cs
+// Source: https://discussions.unity.com/t/how-to-make-a-readonly-property-in-inspector/75448/4
+	
+using UnityEngine;
+
+public class ShowOnlyAttribute : PropertyAttribute
+{
+	
+}
